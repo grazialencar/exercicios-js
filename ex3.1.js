df456725488a15ed0b5c7e01ab0cfcf7bc061db2
grazialencar <graziellealencar3@gmail.com>
2026-02-12 22:31:25 -0300

    exercicio 3.1 - Utilizando tipos primitivos: null e undefined.

---
+++ ex3.1.js
@@ -0,0 +1,11 @@
+let enderecoEntrega = null;
+let numeroPedido;
+
+console.log(enderecoEntrega);
+console.log(numeroPedido);
+
+enderecoEntrega = "Av Paulista, 123";
+numeroPedido = 456;
+
+console.log("Endereço de entrega: " + enderecoEntrega);
+console.log("Número do pedido: " + numeroPedido);
