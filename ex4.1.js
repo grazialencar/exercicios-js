71cbc780fdd6afea7d348fe08610e2d82e2fe0a8
grazialencar <graziellealencar3@gmail.com>
2026-02-12 22:56:10 -0300

    exercicio 4.1 - Criando e exibindo array pelo índice.

---
+++ ex4.1.js
@@ -0,0 +1,6 @@
+let tarefas = ["Comer", "Trabalhar", "Estudar"]
+console.log(tarefas[0]);
+console.log(tarefas[2]);
+console.log(tarefas.length);
+tarefas.push("Dormir");
+console.log(tarefas.length);
