99022bc9718701828b44815a76be43b8f3173977
grazialencar <graziellealencar3@gmail.com>
2026-02-12 22:20:07 -0300

    exercicio 2.2 - Retornando os tipos de variáveis com typeof.

---
+++ ex2.2.js
@@ -0,0 +1,9 @@
+let nome = "Grazielle Alencar";
+let idade = 18;
+const ativa = true;
+let saldo;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof ativa);
+console.log(typeof saldo);
