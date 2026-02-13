8d6f4144b396e63971a49799ca7d1935a710e3a8
grazialencar <graziellealencar3@gmail.com>
2026-02-12 22:09:45 -0300

    exercicio 2.1 - Utilizando variáveis e exibindo os resultando.

---
+++ ex2.1.js
@@ -0,0 +1,9 @@
+const nomeJogador = "Grazielle Alencar";
+let pontuacao = 0;
+
+console.log("Nome do jogador: " + nomeJogador);
+console.log("Pontuação inicial: " + pontuacao);
+
+pontuacao += 10;
+
+console.log("Pontuação após ganhar 10 pontos: " + pontuacao);
