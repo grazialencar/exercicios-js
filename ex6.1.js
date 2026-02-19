6d67cbefc70d93ed804bede734160a8d0166ff52
grazialencar <graziellealencar3@gmail.com>
2026-02-19 18:57:30 -0300

    exercicio 6.1 - Utilizando a estrutura de repetição for

---
+++ ex6.1.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let alunos = ["Ana", "Bruno", "Carla", "Diego"];
+
+for (let i = 0; i < alunos.length; i++) {
+    console.log(i + ": " + alunos[i]);
+}
