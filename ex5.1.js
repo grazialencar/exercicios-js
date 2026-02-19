58d4053443b1c6a45a647532037fe24ec35d52ea
grazialencar <graziellealencar3@gmail.com>
2026-02-19 18:47:23 -0300

    exercicio 5.1 - Usando tomadores de decisões.

---
+++ ex5.1.js
@@ -0,0 +1,18 @@
+let nota = 8; // Resultado esperado Aprovado
+
+if (nota >= 7) {
+    console.log("Aprovado");
+} else if (nota >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
+
+nota = 2;
+if (nota >= 7) {
+    console.log("Aprovado");
+} else if (nota >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
